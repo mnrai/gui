277831585579340a1ac4aff3dcf6187ac250ffce
Roy Rijkers <royrijkers@live.nl>
2022-09-30 19:21:22 +0200

    fix cookie

--- pages/api/auth/login.ts
+++ pages/api/auth/login.ts
@@ -63,7 +63,7 @@ export default async function handler(
           expiresIn: 60*60,
         }
       );
-      res.setHeader("Set-Cookie", `Authorization=${token}; HttpOnly;  Secure, Path=/; Domain=.runpod.io`);
+      res.setHeader("Set-Cookie", `Authorization=${token}; HttpOnly;  Secure, Path=/; Domain=.proxy.runpod.io`);
       res.status(200).json({ user, comparePW, randomStringHash, token });
     } catch(e) {
           return res.status(401).json({ error: "not authorised" });
