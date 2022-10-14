9f3e344544589468d42196471db387117b1a811a
Roy Rijkers <royrijkers@live.nl>
2022-10-14 16:26:33 +0200

    fix

--- helpers/cmdOptions.ts
+++ helpers/cmdOptions.ts
@@ -2,6 +2,8 @@ const commandLineArgs = require("command-line-args");
 const optionDefinitions = [
   { name: "port", alias: "p", type: Number },
   { name: "use_http", type: Boolean },
+  { name: "telegram_group_id", type: String },
+  { name: "telegram_bot_token", type: String },
 ];
 const options = typeof window !== "undefined" ? {} :commandLineArgs(optionDefinitions);
 export const cmdOptions = options;
